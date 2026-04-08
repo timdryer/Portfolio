93c3881e83cf121aa2bf7a2eb4fde9d4d2601421
Tim Dryer <timdryer@gmail.com>
2026-04-08 12:09:44 -0500

    Enabled https_only=true.

--- webapp/obj/Debug/net8.0/webapp.AssemblyInfo.cs
+++ webapp/obj/Debug/net8.0/webapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51dd7bfd8344cb5dcfeacf0b7173aa0a548eb3a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7518a1934925518769cefc5619f9d64f7986fd7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
